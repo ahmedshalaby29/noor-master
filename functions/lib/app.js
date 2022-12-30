c90713a1e7e56573b0cea6eaeabd1299eb231ae8
ahmedshalaby29 <ahmedshalaby292000@gmail.com>
2022-12-30 10:34:57 +0200

    edited cert path

--- functions/lib/app.js
+++ functions/lib/app.js
@@ -1,10 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const express = require("express");
-var cors = require('cors');
-const path = require('path');
-const fs = require('fs');
-const https = require('https');
+var cors = require("cors");
+const path = require("path");
+const fs = require("fs");
+const https = require("https");
 const save_1 = require("./endpoints/callables/incremental/editSkill/save");
 const submit_1 = require("./endpoints/callables/incremental/editSkill/submit");
 const signForm_1 = require("./endpoints/callables/signForm");
@@ -20,27 +20,31 @@ const saveAll_1 = require("./endpoints/background/saveAll");
 const app = express();
 const port = 5000;
 app.use(cors());
-app.use(express.json({ limit: '50mb' }));
-app.use(save_1.router)
-    .use(signForm_1.router)
-    .use(postSignForm_1.router)
-    .use(newAccount_1.router)
-    .use(navigation_1.router)
-    .use(formOptions_1.router)
-    .use(submit_1.router)
-    .use(save_2.router)
-    .use(submit_2.router)
-    .use(examReport_1.router)
-    .use(skillReport_1.router)
-    .use(saveAll_1.router);
+app.use(express.json({ limit: "50mb" }));
+app
+  .use(save_1.router)
+  .use(signForm_1.router)
+  .use(postSignForm_1.router)
+  .use(newAccount_1.router)
+  .use(navigation_1.router)
+  .use(formOptions_1.router)
+  .use(submit_1.router)
+  .use(save_2.router)
+  .use(submit_2.router)
+  .use(examReport_1.router)
+  .use(skillReport_1.router)
+  .use(saveAll_1.router);
 app.get("/", (req, res) => {
-    res.send("Express + TypeScript Server");
+  res.send("Express + TypeScript Server");
 });
-const sslServer = https.createServer({
-    key: fs.readFileSync(path.join('C:\\noor-master\\functions', 'cert', 'private.pem')),
-    cert: fs.readFileSync(path.join('C:\\noor-master\\functions', 'cert', 'certificate.pem')),
-}, app);
+const sslServer = https.createServer(
+  {
+    key: fs.readFileSync(path.join(`${__dirname}`, "cert", "private.pem")),
+    cert: fs.readFileSync(path.join(`${__dirname}`, "cert", "certificate.pem")),
+  },
+  app
+);
 sslServer.listen(port, () => {
-    console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
+  console.log(`⚡️[server]: Server is running at https://localhost:${port}`);
 });
 //# sourceMappingURL=app.js.map
